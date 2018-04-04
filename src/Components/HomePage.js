10d04d8ab09c8fa3505588937d5fb5940a3b4ebf
Jun Yan <stephanieyan59@gmail.com>
2018-04-04 11:28:32 -0400

    feat: update HomePage component, add PropType

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,19 +1,29 @@
 import React, {Component} from 'react';
 import BookShelf from './BookShelf.js';
 import {Link} from 'react-router-dom';
+import PropTypes from "prop-types";
 
 class HomePage extends Component {
+    static propTypes = {
+        shelfTitle: PropTypes.string.isRequired,
+        books: PropTypes.array.isRequired,
+        onChangeShelf: PropTypes.func.isRequired
+    };
+
     render() {
         return (
             <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
                 <div className="list-books-content">
                     <div>
-                        <BookShelf shelfTitle='Currently Reading' books={this.props.currentlyReading} onMoveBook={this.props.onMoveBook}/>
-                        <BookShelf shelfTitle='Want to Read' books={this.props.wantToRead} onMoveBook={this.props.onMoveBook}/>
-                        <BookShelf shelfTitle='Read' books={this.props.read} onMoveBook={this.props.onMoveBook}/>
+                        <BookShelf shelfTitle='Currently Reading'
+                            books={this.props.currentlyReading}
+                            onChangeShelf={this.props.onChangeShelf}/>
+                        <BookShelf shelfTitle='Want to Read'
+                            books={this.props.wantToRead}
+                            onChangeShelf={this.props.onChangeShelf}/>
+                        <BookShelf shelfTitle='Read'
+                            books={this.props.read}
+                            onChangeShelf={this.props.onChangeShelf}/>
                     </div>
                 </div>
                 <div className="open-search">
